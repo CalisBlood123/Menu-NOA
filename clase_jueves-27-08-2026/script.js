77d0f7181d4826c5ca94cdb58e2fc420e0d46ce5
CalisBlood123 <caliblood123@gmail.com>
2026-08-27 17:58:55 -0300

    JS

--- clase_jueves-27-08-2026/script.js
+++ clase_jueves-27-08-2026/script.js
@@ -1,1 +1,55 @@
 console.log("Hello World")
+
+let listaSuper = [
+    {
+        producto: "leche",
+        tipo: "deslactosada",
+        cantidad: 1
+    },
+
+    {
+        producto: "harina",
+        tipo: "000",
+        cantidad: 5
+    },
+
+    {
+        producto: "papel higienico",
+        tipo: "comun",
+        cantidad: 72
+    }
+]
+
+//Metodos forEach
+//Metodo funcion común
+
+listaSuper.forEach(
+    function (items) {
+        const name = (4 ** 4);
+        console.log(name);
+    }
+)
+
+//Filter (Se usa bastante)
+//return es un indicador de lo que tiene que retornar
+//se tiene que guardar en una constante
+
+const cantidadMenores = listaSuper.filter(
+    function(lista){
+        return lista.cantidad > 2;
+    }
+)
+
+console.log(cantidadMenores)
+
+//Metodo Map
+//Ejemplo: tocar el array base con el item que defina
+
+listaSuper.map (
+    function(compra){
+        compra.precio = 100
+        return compra
+    }
+)
+
+console.log(listaSuper)
