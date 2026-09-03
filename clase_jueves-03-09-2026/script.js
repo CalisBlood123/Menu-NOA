cdaf48f3a242a111721ebb0af23cfe9892a03402
CalisBlood123 <caliblood123@gmail.com>
2026-09-03 18:17:40 -0300

    clase JS.3

--- clase_jueves-03-09-2026/script.js
+++ clase_jueves-03-09-2026/script.js
@@ -1,8 +1,10 @@
-const email = document.getElementById("email");
+//const email = document.getElementById("email");
 
 //oninput (para que se actualize a medida que cambia) onchange (para que cambie al sacar el focus) onclick (para que cambie cada que se le de click)
 
 //event.preventDefault (event puede ser cualquier nombre) (preventDefault es para quitar la accion que tiene por defecto, un boton en el input funciona como submit por defecto)
+
+//onsubmit="darDato(event)"
 
 const darDato = (event) => {
     event.preventDefault();
@@ -10,3 +12,20 @@ const darDato = (event) => {
     const emailData = email.value;
     console.log(emailData);
 }
+
+//ALMACENAMIENTO EN EL NAVEGADOR
+
+//LOCAL STORAGE
+
+const email = document.getElementById("email");
+const pass = document.getElementById("password");
+
+const guardarLocal = () => {
+    const emailData = email.value;
+    const passData = pass.value;
+
+    localStorage.setItem("emailUsuario", emailData);
+    localStorage.setItem("passlUsuario", passData);
+}
+
+//esto se guarda en aplicaciones/almacenamientoLocal (localStorage.setItem("clave", valor))
